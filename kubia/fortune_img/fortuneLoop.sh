a31a81e9f53881f200b047dddf7eeed783328a37
mesri <abde_mes@yahoo.fr>
2023-01-13 16:56:14 +0100

    getting the value of INTERVAL from ENV

--- kubia/fortune_img/fortuneLoop.sh
+++ kubia/fortune_img/fortuneLoop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 t#!/bin/bash
 trap "exit" SIGINT
-INTERVAL=$1
+# INTERVAL=$1  we remove this ligne because w'll put the value of INTERVAL in ENV_VAR
 echo Configured to generate new fortune every $INTERVAL seconds
 mkdir -p /var/htdocs
 while :
 do
  echo $(date) Writing fortune to /var/htdocs/index.html
  /usr/games/fortune > /var/htdocs/index.html
- sleep $INTERVAL
+ sleep $INTERVAL   # in this ligne the script wil get the value naturally wi don't need to get
+                   # INTERVAL ENV_VAR explicitly
 done
